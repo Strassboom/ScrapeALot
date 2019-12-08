69b2f83cad57254ed6e32edccca9bb82e8ce921b
Strassboom <maddog241996@gmail.com>
2019-12-08 00:02:40 -0500

    Successfully created default tables

--- databaseAgent.py
+++ databaseAgent.py
@@ -9,48 +9,38 @@ class BaseTool:
         self.client = bigquery.Client()
         self.dataset_id = ""
 
+    # Executes when instance defined at start of 'with' block
     def __enter__(self):
         return self
 
+    # Executes when instance defined in 'with' block is removed from memory
     def __exit__(self, type, value, tb):
         for dataset_id in [d.dataset_id for d in self.client.list_datasets(self.client.project)]:
             self.client.delete_dataset(dataset_id, delete_contents=True, not_found_ok=True)  # Make an API request.
-            print("Deleted dataset '{}'.".format(dataset_id))
+            print("Deleted dataset '{}'".format(dataset_id))
 
-    def __setDatasetId__(self, customName=arrow.now().format("YY-MM-DD-HH-mm-ss")):
-        candidate_dataset_id = "{}.{}".format(self.client.project,customName.replace(" ",""))
-        dataset_exists = lambda x: True if x in [d.dataset_id for d in self.client.list_datasets(self.client.project)] else False
-        if dataset_exists(customName):
-            self.dataset_id = customName
-        else:
-            print("Dataset with id {} does not exist yet".format(candidate_dataset_id))
-
-    def getDatasetId(self):
-        return self.dataset_id
-
-    def createDataset(self,customName=arrow.now().format("YY-MM-DD-HH-mm-ss"),country="US"):
+    # creates a new dataset with an option for custom naming
+    def createDataset(self,customName=str(arrow.now().timestamp),country="US"):
         dataset_id = "{}.{}".format(self.client.project,customName.replace(" ",""))
         dataset_already_exists = lambda x: True if x in [d.dataset_id for d in self.client.list_datasets(self.client.project)] else False
         if dataset_already_exists(customName):
-            print("Dataset with id {} already exists".format(dataset_id))
+            print("Dataset {} already exists".format(dataset_id))
         else:
             dataset = bigquery.Dataset(dataset_id)
             dataset.location = country
             dataset = self.client.create_dataset(dataset)
-            print("Created dataset with id {}".format(dataset.dataset_id))
+            print("Created dataset {}".format(dataset.dataset_id))
+            return customName
 
+    # attempts to change the current mounted dataset
     def setDataSet(self,customName):
-        # Set dataset_id to the ID of the dataset to fetch.
-        candidate_dataset_id = "{}.{}".format(self.client.project,customName.replace(" ",""))
+        #candidate_dataset_id = "{}.{}".format(self.client.project,customName.replace(" ",""))
         dataset_exists = lambda x: True if x in [d.dataset_id for d in self.client.list_datasets(self.client.project)] else False
         if dataset_exists(customName):
             self.dataset_id = customName
             self.dataset = self.client.get_dataset(self.dataset_id)
         else:
-            print("Dataset with id {} does not exist yet".format(candidate_dataset_id))
-
-    def getDataSet(self):
-        return self.dataset
+            print("Dataset {} does not exist yet".format(customName))
 
     def outputDataSet(self):
         if self.dataset:
@@ -65,7 +55,7 @@ class BaseTool:
             # View dataset properties.
             print("Description: {}".format(self.dataset.description))
             print("Labels:")
-            labels = dataset.labels
+            labels = self.dataset.labels
             if labels:
                 for label, value in labels.items():
                     print("\t{}: {}".format(label, value))
@@ -74,68 +64,36 @@ class BaseTool:
         else:
             print("No dataset loaded")
 
-    def createDefaultTables(self,dataset):
-        # Creates lists of queries that will create the tables we want
-        createTerminals = self.client.query("""
-                                        CREATE
-                                            TABLE
-                                                Terminals(
-                                                    ID int UNIQUE,
-                                                    Name text UNIQUE
-                                                )""")
+    def createDefaultTables(self):
+        tables = [ 
+                    ["Terminals",[["ID","INT64","REQUIRED"],["Name","STRING","REQUIRED"]]],
 
-        createEntryTimes = self.client.query("""
-                                        CREATE
-                                            TABLE
-                                                EntryTimes(
-                                                    ID text UNIQUE,
-                                                    Year int NOT NULL,
-                                                    Month int NOT NULL,
-                                                    Day int NOT NULL,
-                                                    Hour int NOT NULL,
-                                                    Minute int NOT NULL,
-                                                    Second int NOT NULL
-                                                )""")
+                    ["EntryTimes",[["ID","INT64","REQUIRED"],["Date","STRING","REQUIRED"]]],
+                    
+                    ["EntryInfo",[["EntryTimeID","INT64","REQUIRED"],["TerminalID","INT64","REQUIRED"],["Capacity","FLOAT64","REQUIRED"]]]
+                ]
+        
+        for tableInfo in tables:
+            self.createTable(tableInfo)
 
-        createEntries = self.client.query("""
-                                    CREATE
-                                        TABLE
-                                            Entries(
-                                                ID int UNIQUE,
-                                                EntryTimeID text NOT NULL FOREIGN KEY REFERENCES EntryTimes.ID,
-                                                TerminalID int NOT NULL FOREIGN KEY REFERENCES Terminals.ID,
-                                                Capacity double NOT NULL
-                                            )""")
+    def createTable(self,tableInfo):
+        tableName = tableInfo[0]
+        table_exists = lambda x: True if x in [d.table_id for d in self.client.list_tables(self.dataset)] else False
+        if table_exists(tableName):
+            print("Table {} Already exists".format(tableName))
+        else:
+            table_id = "{}.{}.{}".format(self.client.project,self.dataset_id,tableName)
+            schema = []
+            for column in tableInfo[1]:
+                schema.append(bigquery.SchemaField(*column))
 
-        createTableQueries = [createTerminals,createEntryTimes,createEntries]
+            table = bigquery.Table(table_id, schema=schema)
 
-    def createTable(self,customName):
-        table_id = "{}.{}.{}".format(self.client.project,self.dataset_id,customName)
-        schema = [
-        bigquery.SchemaField("ID", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("Name", "STRING", mode="REQUIRED"),
-        ]
+            table = self.client.create_table(table)
 
-        table = bigquery.Table(table_id, schema=schema)
-        table = self.client.create_table(table)  # Make an API request.
-        print(
-            "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
-        )
-
-    def getTables(self,dataset):
-       return list(self.client.list_tables(dataset))
-
-    def getTable(self,dataset,table_id):
-        self.client.get_table(table_id)
-
-    def insertTableRows(self,table_id,rows):
-        #table_id = "your-project.your_dataset.your_table"
-        table = self.client.get_table(table_id)  # Make an API request.
-        rows_to_insert = rows
-
-        errors = self.client.insert_rows(table, rows_to_insert)  # Make an API request.
-        if errors == []:
-            print("New rows have been added.")
+            print(
+                    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+                )
 
     def outputTables(self,tables):
         print("Tables:")  # Make an API request(s).
@@ -145,11 +103,12 @@ class BaseTool:
         else:
             print("\tThis dataset does not contain any tables.")
 
+
 def fullOperation():
     with BaseTool() as bqClient:
-        bqClient.createDataset("MyFirstDataset")
-        bqClient.setDataSet("MyFirstDataset")
-        bqClient.createDefaultTables(bqClient.dataset)
+        bqClient.dataset_id = bqClient.createDataset()
+        bqClient.setDataSet(bqClient.dataset_id)
+        bqClient.createDefaultTables()
     pass
 
 if __name__ == "__main__":
